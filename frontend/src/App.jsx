8f259d144d54fd2f658a3a2f6e21d635ae36cd95
Souptik Taran <mr.souptiktaran@gmail.com>
2024-10-20 02:43:41 +0530

    Added login/signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes , Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CodeEditor from './pages/CodeEditor';
 // import StudentDashboard from './pages/student';
@@ -16,31 +16,31 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" >
-            <Route index element={<Home />} />
-            {/* <Route path='CodeEdit' element={<CodeEditor/>}/> */}
-            <Route path="students" element={null}>
-                <Route path='signin' element={<Login/>}/>
-                <Route element={<PrivateRoute role="students"/>}>
-                    
-                    <Route element={<StudentLayout/>}>
-                        <Route path='dashboard' element={<Dashboard/>}/>
-                        <Route path='labs' element={<LabSection/>}/>
-                            {/* <Route index element={<LabSection/>}/> */}
-                        
-                        
-                        <Route path='assignment' element={<StudentLayout/>}/>
-                       
-                    </Route>
-                    <Route path='labs/CodeEditor' element={<CodeEditor/>}/>
-                    {/* <Route path='signout' element={<Logout/>}/> */}
-                </Route>
-                
+          <Route index element={<Home />} />
+          {/* <Route path='CodeEdit' element={<CodeEditor/>}/> */}
+          <Route path="students" element={null}>
+            <Route path='signin' element={<Login />} />
+            <Route element={<PrivateRoute role="students" />}>
+
+              <Route element={<StudentLayout />}>
+                <Route path='dashboard' element={<Dashboard />} />
+                <Route path='labs' element={<LabSection />} />
+                {/* <Route index element={<LabSection/>}/> */}
+
+
+                <Route path='assignment' element={<StudentLayout />} />
+
+              </Route>
+              <Route path='labs/CodeEditor' element={<CodeEditor />} />
+              {/* <Route path='signout' element={<Logout/>}/> */}
             </Route>
-            <Route path="teacherDashboard" element={<TeacherDashboard/>}/>
-            {/* <Route path="students"> */}
-            </Route>
-            {/* Updated path for StudentDashboard */}
-            {/* <Route path="teacherDashboard/*" element={<TeacherDashboard />} /> Updated path for StudentDashboard */}
+
+          </Route>
+          <Route path="teacherDashboard" element={<TeacherDashboard />} />
+          {/* <Route path="students"> */}
+        </Route>
+        {/* Updated path for StudentDashboard */}
+        {/* <Route path="teacherDashboard/*" element={<TeacherDashboard />} /> Updated path for StudentDashboard */}
         {/* </Route> */}
 
       </Routes>
